d1731908c4cb23a6b003165b4d194ac07d23c115
Victorpb02 <vicpebarragan@espol.edu.ec>
2020-11-15 18:04:52 -0500

    Creacion de clases base

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package proyecto1parcial;
+package Main;
 
 /**
  *
  * @author Personal
  */
-public class Proyecto1Parcial {
+public class Main {
 
     /**
      * @param args the command line arguments
